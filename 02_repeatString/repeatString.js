3d4abc144988b4c8f504a7b15f2a7217831acea9
ಠ_ಠ <71596454+strangerwalksamongus@users.noreply.github.com>
2022-06-15 23:21:01 +0300

    ok

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,26 @@
-const repeatString = function() {
+// const repeatString = function(string1, number) {
+
+// let finalResult = string1.repeat(number);
+// return finalResult;
+
+// };
+
+// // Do not edit below this line
+// module.exports = repeatString;
+
+//======================================================//
+
+// let string1 = "";
+// let result;
+const repeatString = function(string1, number) {
+    let result = "";
+    if (number < 0) return "ERROR";
+    
+    for (let i = 0; i < number; i++) {
+        result += string1;  
+    }
+    return result;
+
 
 };
 
