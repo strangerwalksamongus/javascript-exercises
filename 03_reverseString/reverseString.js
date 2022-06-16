3936987889d314dbd66190d1f244d67f3973f3be
ಠ_ಠ <71596454+strangerwalksamongus@users.noreply.github.com>
2022-06-16 20:44:54 +0300

    reverse string complete 2 ways

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,24 @@
-const reverseString = function() {
+// loop only
+// let newStr = ""; // if declared globally, return will store it here, to be used when the function gets called again (NOT WANTED IN THIS CASE)
+
+// const reverseString = function(str) {
+//     let newStr = "";
+
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         newStr += str[i];
+//     }
+//     return newStr;
+
+// };
+
+// substring array
+
+const reverseString = function(str) {
+
+    let toArray = str.split("");
+    let reverseArray = toArray.reverse();
+    let newStr = reverseArray.join("");
+    return newStr;
 
 };
 
