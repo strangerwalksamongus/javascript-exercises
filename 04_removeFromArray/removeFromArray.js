960e6907fc0ab7079e43a8f3d8e859fa08291d92
ಠ_ಠ <71596454+strangerwalksamongus@users.noreply.github.com>
2022-06-17 20:45:18 +0300

    not my day :(

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove) {
+    let newArr = [];
+    let result = "";
+    for (remove of arr) {
+        newArr = arr - remove;
+        result = newArr.join("");
+    }
+    return result;
+
 
 };
 
