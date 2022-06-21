b30adf3e034c072f2f9a966840a73d750b29204b
ಠ_ಠ <71596454+strangerwalksamongus@users.noreply.github.com>
2022-06-21 19:21:30 +0300

    strange order dunno

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a > b) {
+        let temp = b;
+        b = a;
+        a = temp;
+    }
+    else if ((Number.isInteger(a) == false) || (Number.isInteger(b) == false)) return "ERROR";
+
+    for (let i = a; i <= b; i++) {
+        if (a < 0 || b < 0) return ("ERROR");
+
+        else sum += i;
+    }
+    return sum;
+
+
 
 };
 
