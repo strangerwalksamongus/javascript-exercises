c1c7667281e8000267561641eda5325dfd2c71d3
ಠ_ಠ <71596454+strangerwalksamongus@users.noreply.github.com>
2022-06-22 14:35:23 +0300

    ok

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // input number divisible by 4 and NOT 100
+    // if / by 4 AND 100 it ALSO HAS TO BE / 400
+
+    if (year % 4 == 0 && year % 100 != 0) return true;
+    else if (year % 4 != 0 || year % 4 == 0 && year % 100 == 0 && year % 400 != 0) return false;
+    else return true;
 
 };
 
