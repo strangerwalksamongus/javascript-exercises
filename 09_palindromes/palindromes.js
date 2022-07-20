eb6f7ac92a0d08774b10a3ec870c47f78d25ffbc
ಠ_ಠ <71596454+strangerwalksamongus@users.noreply.github.com>
2022-07-20 21:06:40 +0300

    yuh ok

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (inputValue) {
+
+let newArr = inputValue.toLowerCase().replace(/[!?.,&^%$#*(){}\s]/g, ""); // add the symbols to remove between the [] -> THE SPACE IS \s
+// console.log(newArr);
+
+return newArr.split("").reverse().join("") == newArr;
 
 };
 
