aa0e10ed11046c64567d5615a1c9dac0c0fe89c0
ಠ_ಠ <71596454+strangerwalksamongus@users.noreply.github.com>
2022-07-22 18:50:55 +0300

    y

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let arr = [1, 1];
+    
+    for (let i = 1; i < number; i++) {
+        let lastEntry = arr[arr.length - 1] + arr[arr.length - 2];
+        arr.push(lastEntry);
+        console.log(arr);
+    };
+    if (number >= 0) {
+        return arr[number - 1];
+    }
+    else return "OOPS"
 
 };
 
