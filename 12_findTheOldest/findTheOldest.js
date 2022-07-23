2b164525d8989f2351cbbd6579146496eff058e7
ಠ_ಠ <71596454+strangerwalksamongus@users.noreply.github.com>
2022-07-23 12:33:26 +0300

    i hate this

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce(function(olderPerson, currentPerson) {
+        let oldestAge = findAge(olderPerson.yearOfBirth, olderPerson.yearOfDeath);
+        let currentAge = findAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (oldestAge < currentAge) {
+            return currentPerson;
+        }
+        else return olderPerson;
 
+    })                  /// DOESNT FUCKING WOTK
+    
 };
+
+let findAge = function(born, died) {
+    if (died !== true) {
+        died = new Date().getFullYear();
+    }
+    return died - born;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
